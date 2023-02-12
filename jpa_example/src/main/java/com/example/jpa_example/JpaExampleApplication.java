b9dbac97b8143372ca0368aa793aeba6dd8350f3
thiagovb62@gmail.com <thiagovb62@gmail.com>
2023-02-12 10:57:22 -0300

    começando projeto e conectando ao banco de dados

---
+++ jpa_example/src/main/java/com/example/jpa_example/JpaExampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.jpa_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaExampleApplication.class, args);
+
+		System.out.println("Hello World!");
+	}
+
+}
